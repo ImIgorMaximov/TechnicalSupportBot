1129b40418753251c4ba7cd3e5d4a9f6dc860f2e
std-ext-010-32 <std-ext-010-32@praktikum-services.ru>
2024-07-19 19:12:45 +0300

    Instructions for Private Cloud & Code structuring done

---
+++ keyboard.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func getMainKeyboard() tgbotapi.ReplyKeyboardMarkup {
+	return tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Инструкции по продуктам"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Развертывание продуктов"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Связаться с инженером тех. поддержки"),
+		),
+	)
+}
+
+func getProductInstructionsKeyboard() tgbotapi.ReplyKeyboardMarkup {
+	return tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Частное Облако"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Squadus"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Mailion"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Почта 2"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Стандартный"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Назад"),
+		),
+	)
+}
+
+func getPrivateCloudKeyboard() tgbotapi.ReplyKeyboardMarkup {
+	return tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Системные требования"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Руководство по установке"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Руководство по администрированию"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Назад"),
+		),
+	)
+}
+
+func getInstallationGuideKeyboard() tgbotapi.ReplyKeyboardMarkup {
+	return tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("PGS"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("CO"),
+		),
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Назад"),
+		),
+	)
+}
+
+func getBackKeyboard() tgbotapi.ReplyKeyboardMarkup {
+	return tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Назад"),
+		),
+	)
+}
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 
 	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
 	if botToken == "" {
-		log.Fatal("Переменная окружения /'TELEGRAM_BOT_TOKEN/' не объявлена. Добавьте токен бота")
+		log.Fatal("Переменная окружения 'TELEGRAM_BOT_TOKEN' не объявлена. Добавьте токен бота")
 	}
 
 	//Создание бота
@@ -29,33 +29,7 @@ func main() {
 
 	for update := range updates {
 		if update.Message != nil {
-			switch update.Message.Text {
-			case "/start":
-				welcomeMessage := "Добро пожаловать в чат бот тех. поддержки МойОфис! :) " +
-					"Выберите необходимую функцию:\n" +
-					"1. Инструкции по продуктам.\n" +
-					"2. Связаться с инженером тех. поддержки.\n"
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, welcomeMessage)
-				keyboard := tgbotapi.NewReplyKeyboard(
-					tgbotapi.NewKeyboardButtonRow(
-						tgbotapi.NewKeyboardButton("Инструкции по продуктам"),
-					),
-					tgbotapi.NewKeyboardButtonRow(
-						tgbotapi.NewKeyboardButton("Связаться с инженером тех. поддержки"),
-					),
-				)
-				msg.ReplyMarkup = keyboard
-				bot.Send(msg)
-			case "Инструкции по продуктам":
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Ссылка на инструкции по продуктам - https://support.myoffice.ru/products/\n")
-				msg.ParseMode = "markdown"
-				bot.Send(msg)
-			case "Связаться с инженером тех. поддержки":
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Технический пресейл Игорь - @IgorMaksimov2000 \n")
-				msg.ParseMode = "markdown"
-				bot.Send(msg)
-			}
+			handleUpdate(bot, update)
 		}
 	}
 }
-
