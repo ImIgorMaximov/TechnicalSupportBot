c3ba5d293a8e94c800b55a0db7d5b51c53b18331
Igor <igor.maksimov@myoffice.team>
2024-07-29 15:33:15 +0300

    Added mailion documentation: pgs/handlers/mailionInstructions.go

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -22,6 +22,9 @@ func HandleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	case "Squadus":
 		sendInstructions(bot, chatID, "squadus")
 		previousState[chatID] = "squadus"
+	case "Mailion":
+		sendInstructions(bot, chatID, "mailion")
+		previousState[chatID] = "mailion"
 	case "Системные требования":
 		handleSystemRequirements(bot, chatID)
 	case "Руководство по установке":
@@ -48,7 +51,7 @@ func handleBackButton(bot *tgbotapi.BotAPI, chatID int64) {
 	case "product":
 		sendWelcomeMessage(bot, chatID)
 		previousState[chatID] = "start"
-	case "privateCloud", "squadus":
+	case "privateCloud", "squadus", "mailion":
 		sendProduct(bot, chatID)
 		previousState[chatID] = "product"
 	case "requirementsPrivateCloud", "installationGuidePrivateCloud", "adminGuidePrivateCloud":
@@ -60,6 +63,9 @@ func handleBackButton(bot *tgbotapi.BotAPI, chatID int64) {
 	case "requirementsSquadus", "installationGuideSquadus", "adminGuideSquadus":
 		sendInstructions(bot, chatID, "squadus")
 		previousState[chatID] = "squadus"
+	case "requirementsMailion", "installationGuideMailion", "adminGuideMailion":
+		sendInstructions(bot, chatID, "mailion")
+		previousState[chatID] = "mailion"
 	default:
 		sendWelcomeMessage(bot, chatID)
 		previousState[chatID] = "start"
@@ -73,6 +79,9 @@ func handleSystemRequirements(bot *tgbotapi.BotAPI, chatID int64) {
 	} else if previousState[chatID] == "squadus" {
 		sendSystemRequirementsSquadus(bot, chatID)
 		previousState[chatID] = "requirementsSquadus"
+	} else if previousState[chatID] == "mailion" {
+		sendSystemRequirementsMailion(bot, chatID)
+		previousState[chatID] = "requirementsMailion"
 	}
 }
 
@@ -83,6 +92,9 @@ func handleInstallationGuide(bot *tgbotapi.BotAPI, chatID int64) {
 	} else if previousState[chatID] == "squadus" {
 		sendInstallationGuideSquadus(bot, chatID)
 		previousState[chatID] = "installationGuideSquadus"
+	} else if previousState[chatID] == "mailion" {
+		sendInstallationGuideMailion(bot, chatID)
+		previousState[chatID] = "installationGuideMailion"
 	}
 }
 
@@ -93,5 +105,8 @@ func handleAdminGuide(bot *tgbotapi.BotAPI, chatID int64) {
 	} else if previousState[chatID] == "squadus" {
 		sendAdminGuideSquadus(bot, chatID)
 		previousState[chatID] = "adminGuideSquadus"
+	} else if previousState[chatID] == "mailion" {
+		sendAdminGuideMailion(bot, chatID)
+		previousState[chatID] = "adminGuideMailion"
 	}
 }
