acd470a86a9db13db6e37781c4411c78f334cd27
Igor <igor.maksimov@myoffice.team>
2024-10-01 16:58:52 +0300

    Changing a file pkg/sizing/sizingPrivateCloudStandalone.go

--- pkg/sizing/sizingPrivateCloudStandalone.go
+++ pkg/sizing/sizingPrivateCloudStandalone.go
@@ -12,102 +12,113 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
-// Глобальная переменная для хранения введённых данных от пользователей
+// Определение глобальной переменной для хранения пользовательских вводов по состояниям
 var userInputValues = make(map[int64][]string)
-var previousStateSizingPrivateCloudStandalone = make(map[int64]string)
 
-func HandleSizingPrivateCloudStandalone(bot *tgbotapi.BotAPI, chatID int64) {
-	// Запрос данных у пользователя
-	msg := tgbotapi.NewMessage(chatID, "Введите количество пользователей (Например, 50):")
-	bot.Send(msg)
-	previousStateSizingPrivateCloudStandalone[chatID] = "awaitingUserCountPrivateCloud"
-	log.Printf("Cостояние previousStateSizingPrivateCloudStandalone: %s", previousStateSizingPrivateCloudStandalone[chatID])
-}
+// Глобальная переменная для хранения текущего состояния
+var currentState string
 
-// HandleUserInputPrivateCloud обрабатывает ввод пользователя
-func HandleUserInputPrivateCloud(bot *tgbotapi.BotAPI, chatID int64, userInput string) {
-	log.Println("Текущее состояние перед обработкой ввода:", previousStateSizingPrivateCloudStandalone[chatID])
-	log.Println("Полученное значение от пользователя:", userInput)
+// Обработка ввода пользователя и управление состоянием
+func HandleUserInput(bot *tgbotapi.BotAPI, chatID int64, state *string) {
+	currentState = *state
+	var userInput string // Переменная для хранения ввода пользователя
 
-	switch previousStateSizingPrivateCloudStandalone[chatID] {
-	case "awaitingUserCountPrivateCloud":
-		userInputValues[chatID] = []string{userInput} // Сохраняем первое значение
-		msg := tgbotapi.NewMessage(chatID, "Введите количество одновременно активных пользователей (Например, 10):")
-		bot.Send(msg)
-		previousStateSizingPrivateCloudStandalone[chatID] = "awaitingActiveUserCountPrivateCloud" // Обновляем состояние
-		log.Println("Состояние изменено на awaitingActiveUserCountPrivateCloud")
+	log.Printf("Получен ввод от пользователя. Текущее состояние: %s", currentState)
+
+	switch currentState {
+	case "standalone":
+		log.Printf("Обработка состояния: %s.", currentState)
+		currentState = "awaitingMaxUserCountPrivateCloud"
+		userInputValues[chatID] = []string{} // Инициализация мапы для пользователя
+		HandleNextInput(bot, chatID, userInput, "Введите максимальное количество пользователей (например, 50):", "awaitingMaxUserCountPrivateCloud")
+
+	case "awaitingMaxUserCountPrivateCloud":
+		log.Println("Обработка состояния: awaitingMaxUserCountPrivateCloud")
+		userInputValues[chatID] = append(userInputValues[chatID], userInput) // Сохраняем ввод
+		currentState = "awaitingActiveUserCountPrivateCloud"
+		HandleNextInput(bot, chatID, userInput, "Введите количество одновременно активных пользователей (например, 10):", "awaitingActiveUserCountPrivateCloud")
 
 	case "awaitingActiveUserCountPrivateCloud":
-		userInputValues[chatID] = append(userInputValues[chatID], userInput) // Сохраняем второе значение
-		msg := tgbotapi.NewMessage(chatID, "Введите количество редактируемых документов одновременно (Например, 10):")
-		bot.Send(msg)
-		previousStateSizingPrivateCloudStandalone[chatID] = "awaitingDocumentCountPrivateCloud" // Обновляем состояние
-		log.Println("Состояние изменено на awaitingDocumentCountPrivateCloud")
+		log.Println("Обработка состояния: awaitingActiveUserCountPrivateCloud")
+		userInputValues[chatID] = append(userInputValues[chatID], userInput) // Сохраняем ввод
+		currentState = "awaitingDocumentCountPrivateCloud"
+		HandleNextInput(bot, chatID, userInput, "Введите количество редактируемых документов (например, 200):", "awaitingDocumentCountPrivateCloud")
 
 	case "awaitingDocumentCountPrivateCloud":
-		userInputValues[chatID] = append(userInputValues[chatID], userInput) // Сохраняем третье значение
-		msg := tgbotapi.NewMessage(chatID, "Введите дисковую квоту пользователей в хранилище (ГБ) (Например, 2):")
-		bot.Send(msg)
-		previousStateSizingPrivateCloudStandalone[chatID] = "awaitingStorageQuotaPrivateCloud" // Обновляем состояние
-		log.Println("Состояние изменено на awaitingStorageQuotaPrivateCloud")
+		log.Println("Обработка состояния: awaitingDocumentCountPrivateCloud")
+		userInputValues[chatID] = append(userInputValues[chatID], userInput) // Сохраняем ввод
+		currentState = "awaitingStorageQuotaPrivateCloud"
+		HandleNextInput(bot, chatID, userInput, "Введите дисковую квоту пользователей в хранилище (ГБ) (например, 2):", "awaitingStorageQuotaPrivateCloud")
 
 	case "awaitingStorageQuotaPrivateCloud":
-		userInputValues[chatID] = append(userInputValues[chatID], userInput) // Сохраняем четвертое значение
+		log.Println("Обработка состояния: awaitingStorageQuotaPrivateCloud")
+		userInputValues[chatID] = append(userInputValues[chatID], userInput) // Сохраняем ввод
 		log.Println("Все данные от пользователя получены:", userInputValues[chatID])
 
 		// После получения всех значений выполняем расчет
-		calculateAndSendSizing(bot, chatID)
+		calculateAndSendSizing(bot, chatID, userInputValues[chatID])
 		log.Println("Результаты расчета отправлены пользователю")
 
 	default:
-		msg := tgbotapi.NewMessage(chatID, "Ошибка: неизвестное состояние.")
-		bot.Send(msg)
-		log.Println("Ошибка: состояние неизвестно")
+		log.Printf("Ошибка: Неизвестное состояние или некорректный ввод. Состояние: %s", currentState)
+		sendErrorMessage(bot, chatID, "Ошибка: неизвестное состояние или некорректный ввод.")
+	}
+
+	// Обновление состояния после обработки
+	*state = currentState
+}
+
+// HandleNextInput помогает запрашивать следующие данные и обновлять состояние
+func HandleNextInput(bot *tgbotapi.BotAPI, chatID int64, userInput string, nextMessage string, nextState string) {
+	userInputValues[chatID] = append(userInputValues[chatID], userInput) // Сохраняем текущий ввод
+	msg := tgbotapi.NewMessage(chatID, nextMessage)                      // Создаём сообщение для пользователя
+	if _, err := bot.Send(msg); err != nil {
+		log.Printf("Ошибка при отправке сообщения: %s", err) // Логируем ошибку
 	}
 }
 
-// HandleNextInput помогает запрашивать следующие данные
-func HandleNextInput(bot *tgbotapi.BotAPI, chatID int64, userInput string, nextMessage string, nextState string) {
-	userInputValues[chatID] = append(userInputValues[chatID], userInput)
-	msg := tgbotapi.NewMessage(chatID, nextMessage)
-	bot.Send(msg)
-	previousStateSizingPrivateCloudStandalone[chatID] = nextState
-}
-
 // calculateAndSendSizing выполняет расчет и отправляет результат пользователю
-func calculateAndSendSizing(bot *tgbotapi.BotAPI, chatID int64) {
+func calculateAndSendSizing(bot *tgbotapi.BotAPI, chatID int64, userInputValues []string) {
 	// Открытие файла Excel
 	filePath := "/home/admin-msk/Documents/sizingPrivateCloudPSNStandalone.xlsx"
 	f, err := excelize.OpenFile(filePath)
 	if err != nil {
+		sendErrorMessage(bot, chatID, "Произошла ошибка при открытии файла.")
 		log.Println("Ошибка открытия файла:", err)
-		msg := tgbotapi.NewMessage(chatID, "Произошла ошибка при открытии файла.")
-		bot.Send(msg)
 		return
 	}
 	defer f.Close()
 
 	// Заполнение ячеек данными
-	err = f.SetCellValue("PSN", "D4", userInputValues[chatID][0])
-	err = f.SetCellValue("PSN", "F6", userInputValues[chatID][1])
-	err = f.SetCellValue("PSN", "F7", userInputValues[chatID][2])
-	err = f.SetCellValue("PSN", "D8", userInputValues[chatID][3])
+	err = fillExcelFile(f, userInputValues)
 	if err != nil {
+		sendErrorMessage(bot, chatID, "Произошла ошибка при записи в файл.")
 		log.Println("Ошибка записи в файл:", err)
-		msg := tgbotapi.NewMessage(chatID, "Произошла ошибка при записи в файл.")
-		bot.Send(msg)
 		return
 	}
 
 	// Сохранение изменений
 	if err := f.Save(); err != nil {
+		sendErrorMessage(bot, chatID, "Произошла ошибка при сохранении файла.")
 		log.Println("Ошибка сохранения файла:", err)
-		msg := tgbotapi.NewMessage(chatID, "Произошла ошибка при сохранении файла.")
-		bot.Send(msg)
 		return
 	}
 
 	// Извлечение результатов
+	sendSizingResults(bot, chatID, f)
+}
+
+// fillExcelFile заполняет Excel данными пользователя
+func fillExcelFile(f *excelize.File, userInputValues []string) error {
+	err := f.SetCellValue("PSN", "D4", userInputValues[0]) // Макс. количество пользователей
+	err = f.SetCellValue("PSN", "F6", userInputValues[1])  // Кол-во активных пользователей
+	err = f.SetCellValue("PSN", "F7", userInputValues[2])  // Кол-во редактируемых документов
+	err = f.SetCellValue("PSN", "D8", userInputValues[3])  // Дисковая квота пользователя
+	return err
+}
+
+// sendSizingResults извлекает результаты из Excel и отправляет их пользователю
+func sendSizingResults(bot *tgbotapi.BotAPI, chatID int64, f *excelize.File) {
 	operatorVM, _ := f.GetCellValue("PSN", "C15")
 	operatorCPU, _ := f.GetCellValue("PSN", "D15")
 	operatorRAM, _ := f.GetCellValue("PSN", "E15")
@@ -122,24 +133,8 @@ func calculateAndSendSizing(bot *tgbotapi.BotAPI, chatID int64) {
 	pgsCPU, _ := f.GetCellValue("PSN", "D17")
 	pgsRAM, _ := f.GetCellValue("PSN", "E17")
 
-	value1, err := strconv.ParseFloat(userInputValues[chatID][0], 64)
-	if err != nil {
-		log.Println("Ошибка преобразования строки в число:", err)
-		msg := tgbotapi.NewMessage(chatID, "Ошибка данных: невозможно преобразовать значение в число.")
-		bot.Send(msg)
-		return
-	}
-
-	value2, err := strconv.ParseFloat(userInputValues[chatID][3], 64)
-	if err != nil {
-		log.Println("Ошибка преобразования строки в число:", err)
-		msg := tgbotapi.NewMessage(chatID, "Ошибка данных: невозможно преобразовать значение в число.")
-		bot.Send(msg)
-		return
-	}
-
-	ssdValue := 100 + value1*value2
-	pgsSSD := int(math.Round(ssdValue))
+	// Расчет значения для PGS SSD
+	ssdValue := calculatePGSSSD(userInputValues[0])
 
 	// Отправка результата пользователю
 	resultMsg := fmt.Sprintf(
@@ -149,18 +144,44 @@ func calculateAndSendSizing(bot *tgbotapi.BotAPI, chatID int64) {
 			"Компонент PGS: кол-во ВМ - %s, CPU - %s, RAM - %s ГБ, SSD - %d ГБ.",
 		operatorVM, operatorCPU, operatorRAM, operatorSSD,
 		coVM, coCPU, coRAM, coSSD,
-		pgsVM, pgsCPU, pgsRAM, pgsSSD,
+		pgsVM, pgsCPU, pgsRAM, ssdValue,
 	)
 	msg := tgbotapi.NewMessage(chatID, resultMsg)
 	bot.Send(msg)
 
+	// Отправка клавиатуры с основным меню
+	showMainMenu(bot, chatID)
+}
+
+// calculatePGSSSD вычисляет значение для PGS SSD
+func calculatePGSSSD(userInputValues []string) int {
+	value1, err := strconv.ParseFloat(userInputValues[0], 64) // Количество пользователей
+	if err != nil {
+		log.Println("Ошибка преобразования строки в число:", err)
+		return 0
+	}
+
+	value2, err := strconv.ParseFloat(userInputValues[3], 64) // Дисковая квота
+	if err != nil {
+		log.Println("Ошибка преобразования строки в число:", err)
+		return 0
+	}
+
+	ssdValue := 100 + value1*value2
+	return int(math.Round(ssdValue))
+}
+
+// showMainMenu отправляет клавиатуру с главным меню
+func showMainMenu(bot *tgbotapi.BotAPI, chatID int64) {
 	keyboard := keyboards.GetMainMenuKeyboard()
 	msgWithKeyboard := tgbotapi.NewMessage(chatID, "Выберите следующую опцию:")
 	msgWithKeyboard.ReplyMarkup = keyboard
-	if _, err := bot.Send(msgWithKeyboard); err != nil {
-		log.Printf("Ошибка отправки клавиатуры: %v", err)
-	}
-	// Очистка состояния
-	previousStateSizingPrivateCloudStandalone[chatID] = "sizingResultProvided"
-	userInputValues[chatID] = nil
+	bot.Send(msgWithKeyboard)
+}
+
+// sendErrorMessage отправляет сообщение об ошибке
+func sendErrorMessage(bot *tgbotapi.BotAPI, chatID int64, errorMessage string) {
+	msg := tgbotapi.NewMessage(chatID, errorMessage)
+	bot.Send(msg)
+	log.Println(errorMessage)
 }
